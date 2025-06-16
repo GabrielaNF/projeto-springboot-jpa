575f0446d7342fb4852a507272792ca9ca072fc3
Gabriela <gabrielafonseca@alunos.utfpr.edu.br>
2025-06-16 09:35:37 -0300

    project created

---
+++ src/main/java/com/projetoudemy/sbcourse/SbcourseApplication.java
@@ -0,0 +1,13 @@
+package com.projetoudemy.sbcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoudemy/sbcourse/SbcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoudemy.sbcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
